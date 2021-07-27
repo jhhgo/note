0978944d9d9e5292bd64e2a963a16b4ab3e8333a
jhhgo <jiangtao1024@sogou-inc.com>
2021-07-27 16:22:17 +0800

    垃圾回收

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-var a = require('./a.js')
-var b = require('./b.js')
-console.log('main.js中 a.done=' + a.done + 'b.done=' + b.done);
+const a = require("./a.js");
+const b = require("./b.js");
+console.log("main.js中 a.done=" + a.done + "b.done=" + b.done);
